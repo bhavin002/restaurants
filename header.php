1fdff8189b22527db6a79f4370450e612d65ca36
Parth Patel <100956473+parthpatel1902@users.noreply.github.com>
2023-09-16 10:03:32 +0530

    home page change

--- header.php
+++ header.php
@@ -33,6 +33,7 @@
   <!-- Template Main CSS File -->
   <link href="assets/css/main.css" rel="stylesheet">
   <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 </head>
 
 
